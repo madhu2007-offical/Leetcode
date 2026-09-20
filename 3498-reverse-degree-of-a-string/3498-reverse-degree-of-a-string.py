2ce91590898b9cfc17f137ac0698b2d311bb3541
Madhumitha K <madhumithak294@gmail.com>
2026-09-20 21:36:42 +0530

    Time: 15 ms (24.72%), Space: 12.2 MB (89.67%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
@@ -0,0 +1,10 @@
+class Solution(object):
+
+    def reverseDegree(self, s):
+        ans = 0
+
+        for i in range(len(s)):
+            reverse_pos = 26 - (ord(s[i]) - ord('a'))
+            ans += reverse_pos * (i + 1)
+
+        return ans
